c0a7c26bd1e03ad961340ae99ae31ff13a927c05
bellycose <77378018+bellycose@users.noreply.github.com>
2022-02-24 23:35:05 -0800

    Uploaded initial test file

---
+++ 2.Search_by_clicks_no_typing/test_noclick.py
@@ -0,0 +1,13 @@
+#Test module
+
+def test_phrase():
+
+    #Given the homepage is displayed
+
+    #When user click on an button
+
+    #Then the joke is generated
+
+    #And the box container expand to fit the text
+
+    raise Exception("Incomplete Test")
