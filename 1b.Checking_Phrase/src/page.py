bfba505aaf706ab7843a48141546edad32dc0382
bellycose <77378018+bellycose@users.noreply.github.com>
2022-02-25 19:33:45 -0800

    Uploaded new contents. Require Web Location

---
+++ 1b.Checking_Phrase/src/page.py
@@ -0,0 +1,21 @@
+#Element
+
+class SearchPhrase:
+
+    def __init__(self,broswer):
+        self.browser = broswer
+
+    def load(self):
+        pass
+
+    def search(self,phrase):
+        pass
+
+#Identifying the locator of the search bar
+#Locators--
+
+#Search Bar: id sb_form_q 
+
+#Search Bar text: b_searchbox
+#Query link: b_title
+#Query name: 
---
+++ 1b.Checking_Phrase/src/phrase.py
@@ -0,0 +1,15 @@
+#Locator
+
+class ResultPhrase:
+
+    def __init__(self, broswer):
+        self.browser = broswer
+
+    def result_link_titles(self):
+        return []
+
+    def search_input_value(self):
+        return ""
+
+    def title(self):
+        return ""
