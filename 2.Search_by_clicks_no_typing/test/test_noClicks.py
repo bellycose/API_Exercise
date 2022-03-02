7798a9157bd6c230c9daeb21f7d3dfebda385cc0
bellycose <77378018+bellycose@users.noreply.github.com>
2022-03-01 22:00:36 -0800

    Replace with new and completed code.

--- 2.Search_by_clicks_no_typing/test/test_noClicks.py
+++ 2.Search_by_clicks_no_typing/test/test_noClicks.py
@@ -1,25 +1,31 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 #Feature: Joke Generator Button
 
 #Scenario: Click button to move onto next jokes.
-def test_joke():
-    
-    #Given the website is displayed with the instruction and button
-    b=webdriver.Chrome()
-    b.get("https://elated-benz-84557d.netlify.app/#")
-    b.implicitly_wait(10)
-    title = "Random Jokes"
-    assert title == b.title
-    #When the user clicked on the button
-    l=b.find_element(By.ID,"next")
-    l.click()
-    #The the joke gets generated
-    b.find_element(By.ID,"p")
 
+def test_joke():
+
+#Given the website is displayed with the instruction and button
+    b=webdriver.Chrome()
+    wait = WebDriverWait(b, 20)
+
+    b.get("https://elated-benz-84557d.netlify.app/#")
     
+#When the user clicked on the button
+    wait.until(EC.visibility_of_element_located((By.ID, "next"))).click()
+
+#Then the joke gets generated
+    m = wait.until(EC.visibility_of_element_located((By.XPATH, "//*[@id='p']"))).text
+    if m == "":
+        assert False
+    else:
+        assert True
+
+
     b.quit()
 
 test_joke()
